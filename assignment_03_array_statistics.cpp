6a140c6e38f31427ef002b9c23faa68a9ec28f9a
drezylord <drezylord@gmail.com>
2026-07-30 16:55:25 +0000

    A C++ program that reads a collection of numbers from the user and computes key statistical values using separate function

--- assignment_03_array_statistics.cpp
+++ assignment_03_array_statistics.cpp
@@ -42,3 +42,96 @@
 #include <iostream>
 using namespace std;
 
+
+int calculateSum(int number_list[], int number_count)
+{
+    int total_value = 0;
+
+    for (int number_position = 0; number_position < number_count; number_position++)
+    {
+        total_value = total_value + number_list[number_position];
+    }
+
+    return total_value;
+}
+
+
+double calculateAverage(int number_list[], int number_count)
+{
+    int total_value = calculateSum(number_list, number_count);
+
+    double average_value = static_cast<double>(total_value) / number_count;
+
+    return average_value;
+}
+
+
+int findMaximum(int number_list[], int number_count)
+{
+    int maximum_value = number_list[0];
+
+    for (int number_position = 1; number_position < number_count; number_position++)
+    {
+        if (number_list[number_position] > maximum_value)
+        {
+            maximum_value = number_list[number_position];
+        }
+    }
+
+    return maximum_value;
+}
+
+
+int findMinimum(int number_list[], int number_count)
+{
+    int minimum_value = number_list[0];
+
+    for (int number_position = 1; number_position < number_count; number_position++)
+    {
+        if (number_list[number_position] < minimum_value)
+        {
+            minimum_value = number_list[number_position];
+        }
+    }
+
+    return minimum_value;
+}
+
+
+int main()
+{
+    int number_count;
+
+    cout << "How many numbers? ";
+    cin >> number_count;
+
+    if (number_count <= 0)
+    {
+        cout << "Error: The number must be a positive integer." << endl;
+        return 0;
+    }
+
+    int* entered_numbers = new int[number_count];
+
+    for (int number_position = 0; number_position < number_count; number_position++)
+    {
+        cout << "Enter number " << number_position + 1 << ": ";
+        cin >> entered_numbers[number_position];
+    }
+
+    int total_result = calculateSum(entered_numbers, number_count);
+    double average_result = calculateAverage(entered_numbers, number_count);
+    int maximum_result = findMaximum(entered_numbers, number_count);
+    int minimum_result = findMinimum(entered_numbers, number_count);
+
+    cout << endl;
+    cout << "Results:" << endl;
+    cout << "Sum:     " << total_result << endl;
+    cout << "Average: " << average_result << endl;
+    cout << "Maximum: " << maximum_result << endl;
+    cout << "Minimum: " << minimum_result << endl;
+
+    delete[] entered_numbers;
+
+    return 0;
+}
